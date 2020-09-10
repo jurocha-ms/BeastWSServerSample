267684bf978410ac1db418352fcf7ea803cb6a78
Julio C. Rocha <julio.rocha@microsoft.com>
2020-09-09 20:08:20 -0700

    Finish client implementation.
    Tested against echo.websocket.org.

--- WSClient/ClientMain.cpp
+++ WSClient/ClientMain.cpp
@@ -2,33 +2,48 @@
 #include <winrt/Windows.Networking.Sockets.h>
 #include <winrt/Windows.Storage.Streams.h>
 
+#include <Windows.h>
+
+#include <chrono>
 #include <iostream>
 
 using namespace winrt::Windows::Foundation;
 using namespace winrt::Windows::Networking::Sockets;
 using namespace winrt::Windows::Storage::Streams;
 
-using std::cout;
 using std::cerr;
+using std::cout;
 using std::endl;
+using std::wcerr;
+using std::wcout;
 
 auto s_webSocket = MessageWebSocket {};
+winrt::handle s_readEvent { ::CreateEvent(nullptr, true, false, nullptr) };
 
 void OnMessageReceived(MessageWebSocket const& sender, MessageWebSocketMessageReceivedEventArgs const& args)
 {
-	auto dataReader { args.GetDataReader() };
-	dataReader.UnicodeEncoding(UnicodeEncoding::Utf8);
+	auto reader { args.GetDataReader() };
+	reader.UnicodeEncoding(UnicodeEncoding::Utf8);
 
-	auto message = dataReader.ReadString(dataReader.UnconsumedBufferLength());
+	auto message = reader.ReadString(reader.UnconsumedBufferLength());
+	wcout << L"[SERVER] " <<  message.c_str() << endl;
 
-	cout << L"[SERVER] " <<  message.c_str() << endl;
-
-	s_webSocket.Close();
+	::SetEvent(s_readEvent.get());
+	s_readEvent.close();
 }
 
 void OnClosed(IWebSocket const&, WebSocketClosedEventArgs const& args)
 {
-	cout << L"[CLOSED] Code: " << args.Code() << " Reason: " << args.Reason().c_str() << endl;
+	wcout << L"[CLOSED] Code: " << args.Code() << L" Reason: " << args.Reason().c_str() << endl;
+}
+
+IAsyncAction SendMessageAsync(std::wstring message)
+{
+	DataWriter writer { s_webSocket.OutputStream() };
+	writer.WriteString(message);
+
+	co_await writer.StoreAsync();
+	writer.DetachStream();
 }
 
 IAsyncAction MainAsync()
@@ -41,12 +56,18 @@ IAsyncAction MainAsync()
 	{
 		co_await s_webSocket.ConnectAsync(Uri { L"wss://localhost:5556" });
 		//co_await s_webSocket.ConnectAsync(Uri{L"wss://echo.websocket.org"});
-
 		cout << "[CONNECTED] " << endl;
+
+		co_await SendMessageAsync(L"HELLO!");
+		cout << "[SENT]" << endl;
+
+		co_await winrt::resume_on_signal(s_readEvent.get()/*, std::chrono::seconds(5)*/);
+
+		s_webSocket.Close();
 	}
 	catch (winrt::hresult_error& e)
 	{
-		cerr << L"[ERROR] " << e.message().c_str() << endl;
+		wcerr << L"[ERROR] " << e.message().c_str() << endl;
 	}
 }
 
